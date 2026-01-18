442b62081afdef1fe5adb418d20c4155ff18b399
dhprlabs <dhprlabs@gmail.com>
2026-01-18 18:42:58 +0530

    add xml launch file and service

--- basic_nodes/src/add_two_ints_client.cpp
+++ basic_nodes/src/add_two_ints_client.cpp
@@ -10,20 +10,29 @@ using namespace std::chrono_literals;
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
 
-    if (argc != 3)
+    // Declare and get parameters
+    node->declare_parameter<int64_t>("a", 0);
+    node->declare_parameter<int64_t>("b", 0);
+    
+    int64_t a = node->get_parameter("a").as_int();
+    int64_t b = node->get_parameter("b").as_int();
+
+    if (a == 0 && b == 0 && argc >= 3)
     {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
-        return 1;
+        a = atoll(argv[1]);
+        b = atoll(argv[2]);
     }
 
-    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Requesting to add: %ld + %ld", a, b);
+
     rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
         node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
 
     auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-    request->a = atoll(argv[1]);
-    request->b = atoll(argv[2]);
+    request->a = a;
+    request->b = b;
 
     while (!client->wait_for_service(1s))
     {
